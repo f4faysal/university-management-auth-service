83a26f223b9ac52e82f6bc38fc18b4a1fa6f3b28
f4faysal <f4faysals@gmail.com>
2023-05-29 00:52:44 +0600

    setup eslint prettier husky lint-staged

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function bootstap() {
   try {
-    await mongoose.connect(config.database__url as string);
+    await mongoose.connect(config.database__url as string)
 
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 
-    console.log(` Database is Connact Success`);
+    console.log(` Database is Connact Success`)
     app.listen(config.port, () => {
-      console.log(`Appliction listening on port ${config.port}`);
-    });
+      console.log(`Appliction listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
-bootstap();
+bootstap()
