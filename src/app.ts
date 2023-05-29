9f589ba1644bf9be9fb6f52d8045cde682e38229
f4faysal <f4faysals@gmail.com>
2023-05-29 12:52:16 +0600

    [DONE]Create User api

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
+import userRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
@@ -8,9 +9,13 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Appliction routs
+
+app.use('/api/v1/users', userRouter)
+
 // testing
-app.get('/', (req: Request, res: Response) => {
-  res.send('University Management server ')
+app.get('/', async (req: Request, res: Response) => {
+  res.send('University Management server Working SuccessFully ')
 })
 
 export default app
--- src/app/modules/users/users.service.ts
+++ src/app/modules/users/users.service.ts
@@ -1,9 +1,16 @@
+import config from '../../../config'
 import { IUser } from './users.interface'
 import { User } from './users.model'
+import { generateUserId } from './users.utils'
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
   // auto generated incremantal id
+  const id = await generateUserId()
+  user.id = id
   // default password
+  if (!user.Password) {
+    user.Password = config.default_user_password as string
+  }
   const createdUsers = await User.create(user)
   if (!createUser) {
     throw new Error('Failed to create User')
