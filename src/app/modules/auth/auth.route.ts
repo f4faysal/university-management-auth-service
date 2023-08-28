044644a5af7d4f6e82725543863a0a6363644aa0
f4faysal <f4faysals@gmail.com>
2023-08-28 23:33:18 +0600

    Optimaige code

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,28 +1,3 @@
-// import express from 'express';
-// import validateRequest from '../../middlewares/validateRequest';
-// import { AuthController } from './auth.controller';
-// import { AuthValidation } from './auth.validation';
-
-// const router = express.Router();
-
-// router.post(
-//   '/login',
-//   validateRequest(AuthValidation.loginZodSchema),
-//   AuthController.loginUser
-// );
-// router.post(
-//   '/refresh-token',
-//   validateRequest(AuthValidation.refreshTokenZodSchema),
-//   AuthController.refreshToken
-// );
-// // router.post(
-// //   '/create-student',
-// //   validateRequest(UserValidation.createdUserZodSchema),
-// //   UserController.createStudent
-// // );
-
-// export const AuthRoutes = router;
-
 import express from 'express';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
