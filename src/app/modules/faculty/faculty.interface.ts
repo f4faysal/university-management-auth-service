9ea818570cc318469c2e8f80cac525ccd6315485
f4faysal <f4faysals@gmail.com>
2023-08-28 23:35:20 +0600

    Optimaigr cone

--- src/app/modules/faculty/faculty.interface.ts
+++ src/app/modules/faculty/faculty.interface.ts
@@ -1,38 +1,3 @@
-// import { Model, Types } from 'mongoose';
-// import { IAcademicDepartment } from '../academicDepartment/academicDepartment.interfaces';
-// import { IAcademicFaculty } from '../academicFaculty/academicFaculty.interfaces';
-
-// export type IFaculty = {
-//   id: string;
-//   name: {
-//     firstName: string;
-//     middleName?: string;
-//     lastName: string;
-//   };
-//   gender: 'male' | 'female';
-//   dateOfBirth: string;
-//   email: string;
-//   contactNo: string;
-//   emergencyContactNo: string;
-//   presentAddress: string;
-//   permanentAddress: string;
-//   bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
-//   designation: 'Professor' | 'Lecturer';
-//   academicDepartment: Types.ObjectId | IAcademicDepartment;
-//   academicFaculty: Types.ObjectId | IAcademicFaculty;
-// };
-
-// export type FacultyModel = Model<IFaculty, Record<string, unknown>>;
-
-// export type IFacultyFilters = {
-//   searchTerm?: string;
-//   id?: string;
-//   bloodGroup?: string;
-//   email?: string;
-//   contactNo?: string;
-//   designation?: string;
-// };
-
 import { Model, Types } from 'mongoose';
 import { IAcademicDepartment } from '../academicDepartment/academicDepartment.interfaces';
 import { IAcademicFaculty } from '../academicFaculty/academicFaculty.interfaces';
--- src/app/modules/faculty/faculty.route.ts
+++ src/app/modules/faculty/faculty.route.ts
@@ -1,25 +1,3 @@
-// import express from 'express';
-// import { FacultyController } from './faculty.controller';
-
-// const router = express.Router();
-
-// // router.get('/:id', FacultyController.getSingleStudent);
-// router.get('/', FacultyController.getAllFaculty);
-// // router.patch(
-// //      '/:id',
-// //      // validateRequest(StudentValidation.studentUpdateZodSchema),
-// //      FacultyController.updateSudent
-// // );
-// // router.delete('/:id', FacultyController.deleteStudent);
-
-// // // router.post(
-// // //   '/create-student',
-// // //   validateRequest(UserValidation.createdUserZodSchema),
-// // //   UserController.createStudent
-// // // );
-
-// export const FacultyRoutes = router;
-
 import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { FacultyController } from './faculty.controller';
